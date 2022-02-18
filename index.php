70e716bc4333709f7b33e49345f73906d7e28ea0
maurojcordoba <mauro.j.cordoba@gmail.com>
2022-02-17 21:38:22 -0300

    CAMBIO

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
-echo "Hola test"; 
-echo "<br>";
-echo "Hola test 2"; 
+
+// CORS
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET');
+
+$rutaBase = 'archivos/';
+
+$ficheros = scandir($rutaBase);
+
+print_r( json_encode($ficheros));
+
